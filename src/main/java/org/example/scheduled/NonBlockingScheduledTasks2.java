7b241fd12998ae584ac46e051dda39be490a18a2
xm.hu <xm.hu>
2025-09-27 19:33:57 +0800

    提交token22

--- src/main/java/org/example/scheduled/NonBlockingScheduledTasks2.java
+++ src/main/java/org/example/scheduled/NonBlockingScheduledTasks2.java
@@ -66,7 +66,7 @@ public class NonBlockingScheduledTasks2 implements CommandLineRunner {
 
     // 全局静态用户ID列表（不可变）
     private static final List<String> USER_IDS = List.of(
-            "60df0298ec7c43bca32248e022667de4", "06024092a9854d45af8991a902bca81a", "42f7a34f04924a4c9c3d9658bf67e09f",  "2e51e301461f46a4ac3b533124b2c008"
+            "1fdfadb113664316b72ba02046fd5cce", "ec236b281413474099abcfa80973baa5", "9b16899c716541449b143d6851e32660",  "7a34170ed1554eb1be56d18f795fd789"
     );
 
     private static final int DEFAULT_USER_INDEX = 1;
--- src/main/java/org/example/scheduled/service/CollectionServiceConstants.java
+++ src/main/java/org/example/scheduled/service/CollectionServiceConstants.java
@@ -5,7 +5,7 @@ public class CollectionServiceConstants {
     public static final String BASE_URL = "https://csmp.df-finance.com.cn";
 
     // 固定Header值
-    public static final String AUTH_TOKEN = "60df0298ec7c43bca32248e022667de4";
+    public static final String AUTH_TOKEN = "1fdfadb113664316b72ba02046fd5cce";
     public static final String CLIENT_ID = "mplanyou";
     public static final String LANG = "ZH_CN";
     public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36";
