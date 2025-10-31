06c94006c007f2f8e394c8259df0a4fccd30d4d9
xianming.hu <78032724@qq.com>
2025-11-01 07:42:02 +0800

    token01

--- src/main/java/org/example/scheduled/service/CollectionServiceConstants.java
+++ src/main/java/org/example/scheduled/service/CollectionServiceConstants.java
@@ -5,7 +5,7 @@ public class CollectionServiceConstants {
     public static final String BASE_URL = "https://csmp.df-finance.com.cn";
 
     // 固定Header值
-    public static final String AUTH_TOKEN = "f1e90a3d73494308837e435f7ed249c6";
+    public static final String AUTH_TOKEN = "a3a5c50702604537951f987d6aca4af5";
     public static final String CLIENT_ID = "mplanyou";
     public static final String LANG = "ZH_CN";
     public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36";
